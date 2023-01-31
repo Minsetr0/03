0384017b6af7761ba3454a44332aa341e374ff63
Minsetr0 <nesterovmaksim71@mail.ru>
2023-01-31 20:21:10 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,7 @@
 ﻿int ascendingCycle = 7;
-// 12-5 = 7, как и вообще все числа там возрастают на 7
-// цикл заканчивается, 96 - 5 = 91, а 91 / 7 = 13, значит цикл должен повторяться 13 раз
-int repeats = 96 / 7;
-int initialNumber = 5;
-for (int i = 0; i <= repeats; i++)
+int repeats = 96;
+
+for (int initialNumber = 5; initialNumber <= repeats; initialNumber += ascendingCycle)
 {
     Console.WriteLine(initialNumber);
-    initialNumber += ascendingCycle;
 }
