2ade3c09c908d2b271f4c46d19e650b7b780cf87
Minsetr0 <nesterovmaksim71@mail.ru>
2023-02-02 21:16:38 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 int repeats = 96;
 int firstNumberInCycle = 5;
 
-for (int initialNumber = firstNumberInCycle; initialNumber <= repeats; initialNumber += ascendingCycle)
+for (int i = firstNumberInCycle; i <= repeats; i += ascendingCycle)
 {
-    Console.WriteLine(initialNumber);
+    Console.WriteLine(i);
 }
